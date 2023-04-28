61c5dd4b8c9f506f6df0f99dbff6557e16f342af
Matilda Flodin <ma4471fl-s@student.lu.se>
2023-04-28 08:55:54 +0200

    compiled intp 1 file

--- as1.hs
+++ as1.hs
@@ -2,4 +2,114 @@
 
 main :: IO()
 main = do
-    putStrLn "Test"
+    contents <- readFile "C:/Users/maddi/Documents/program/haskell/edaf95/labs/src/inconsistent20.txt"
+    let size = length $ head $ lines contents
+    let sudokus = map (parseBoard size . concat) (chunkOf' size $ filter ('=' `notElem`) (lines contents))
+    mapM_ (verifySudoku size) sudokus
+
+chunkOf' :: Int -> [a] -> [[a]]
+chunkOf' _ [] = []
+chunkOf' i ls = take i ls : chunkOf' i (drop i ls)
+
+digitToInt' :: Char -> Int
+digitToInt' c = read [c] :: Int
+
+rows4 :: String
+rows4 = "ABCD"
+cols4 :: String
+cols4 = "1234"
+
+rows9 :: String
+rows9 = "ABCDEFGHI"
+cols9 :: String
+cols9 = "123456789"
+
+cross :: [a] -> [a] -> [[a]]
+cross xs ys = [[x, y] | x <- xs, y <- ys]
+
+board :: Int -> [String]
+board x
+    | x == 4 = cross rows4 cols4
+    | otherwise = cross rows9 cols9
+
+parseBoard :: Int -> String -> [(String, Int)]
+parseBoard x = zip (board x) . map digitToInt'
+
+unitList4 :: [[String]]
+unitList4 = [cross [xs] cols4 | xs <- rows4] ++ --rows
+     --rows
+    [cross rows4 [xs] | xs <- cols4] ++ --cols
+    [cross xs ys | xs <- [take 2 rows4, drop 2 rows4], ys <- [take 2 cols4, drop 2 cols4]] --boxes
+
+unitList9 :: [[String]]
+unitList9 = [cross [xs] cols9 | xs <- rows9] ++ --rows
+     --rows
+    [cross rows9 [xs] | xs <- cols9] ++ --cols
+    [cross xs ys | xs <- [take 3 rows9, take 3 (drop 3 rows9), drop 6 rows9], ys <- [take 3 cols9, take 3 (drop 3 cols9), drop 6 cols9]] --boxes
+
+filterUnitList :: Int -> String -> [[String]]
+filterUnitList x st
+    |x == 4 = filter (elem st) unitList4
+    |otherwise = filter (elem st) unitList9
+
+units :: Int -> [(String, [[String]])]
+units s = [(x, ys)| x <- board s, ys <- [filterUnitList s x]]
+
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates [] = []
+removeDuplicates (x:xs)
+    | x `elem` xs = removeDuplicates xs
+    | otherwise = x : removeDuplicates xs
+
+peers :: Int -> [(String, [String])]
+peers x = zip (board x) $ zipWith (curry func) (board x) (map (removeDuplicates . concat . snd) (units x))
+
+func :: Eq a => (a, [a]) -> [a]
+func tp = filter (/= fst tp) (snd tp)
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe n Nothing = n
+fromMaybe n (Just x) = x
+
+getPeers :: Int -> String -> [String]
+getPeers x st = fromMaybe ["Nothing"] $ lookup st (peers x)
+
+justifyList :: [Maybe a] -> [a]
+justifyList [] = []
+justifyList (x:xs) = [x | Just x <- [x] ++ xs]
+
+lookups :: Eq a => [a] -> [(a, b)] -> [b]
+lookups _ [] = []
+lookups [] _ = []
+lookups (x:xs) tl = justifyList [lookup x tl | x <- [x]++xs]
+
+validSquare :: Int -> (String, Int) -> [(String, Int)] -> Bool
+validSquare x (_, 0) tl = True
+validSquare x tp tl = snd tp `notElem` lookups (getPeers x $ fst tp) tl
+
+reduceList :: (Foldable t, Eq a) => [a] -> t a -> [a]
+reduceList xs ys = [x | x <- xs, x `notElem` ys]
+
+validSquareNumbers :: Int -> (String, Int) -> [(String, Int)] -> (String, [Int])
+validSquareNumbers size (sq, v) xs
+    | (v `elem` [1..size]) && validSquare size (sq, v) xs = (sq, [v])
+    | (v `elem` [1..size]) && not (validSquare size (sq, v) xs) = (sq, [])
+    | otherwise = (sq, reduceList [1..size] (lookups (getPeers size sq) xs))
+
+validBoardNumbers :: Int -> [(String, Int)] -> [(String, [Int])]
+validBoardNumbers x tl = [validSquareNumbers x v tl | v <- tl]
+
+--"010500200900001000002008030500030007008000500600080004040100700000705006003004050"
+
+verifySudoku :: Int -> [(String, Int)] -> IO()
+verifySudoku x tl = do
+    let vs = [validSquare x s tl | s <- tl]
+    let vb = map ((not .null) . snd) (validBoardNumbers x tl)
+    mapM_ (putStrLn . unwords) (chunkOf' x $ map (show . snd) tl)
+    putStrLn "____________"
+    putStrLn "Simple conflicts: False is illegal number"
+    mapM_ (putStrLn . unwords) (chunkOf' x $ map show vs)
+    putStrLn "____________"
+    putStrLn "Blocking conflicts: False squares cannot be filled"
+    mapM_ (putStrLn . unwords) (chunkOf' x $ map show vb)
+    putStrLn "____________"
