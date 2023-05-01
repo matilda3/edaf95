b6df6ebffcd88ed9d1d0a0bdca5d4707b3843419
Matilda Flodin <ma4471fl-s@student.lu.se>
2023-05-01 22:02:06 +0200

    start lab4

---
+++ labs/src/SolveSudoku.hs
@@ -0,0 +1,97 @@
+module SolveSudoku where
+
+-- by Adrian Roth
+
+import Data.Bool
+import Data.Char
+import Data.List
+import Data.Maybe
+
+cross :: [a] -> [a] -> [[a]]
+cross s1 s2 = [[r, c] | r <- s1, c <- s2]
+
+rowBoxes, colBoxes :: [String]
+rowBoxes = ["ABC", "DEF", "GHI"]
+colBoxes = ["123", "456", "789"]
+
+rows, cols :: String
+rows = concat rowBoxes
+cols = concat colBoxes
+
+squares :: [String]
+squares = cross rows cols
+unitlist :: [[String]]
+unitlist = [cross rows [c] | c <- cols]
+        ++ [cross [r] cols | r <- rows]
+        ++ [cross rs cs | rs <- rowBoxes, cs <- colBoxes]
+
+units :: [(String, [[String]])]
+units = [(s, filter (elem s) unitlist) | s <- squares]
+
+peers :: [(String, [String])]
+peers = map (\(s, u) -> (s, delete s (foldl union [] u))) units
+
+--type that represents the board and all possible numbers for each square
+type Board = [(String, [Int])]
+allDigits :: [Int]
+allDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+--the list all Digits repeated infinitely [[], [], ...]
+infAllDigits = repeat allDigits
+emptyBoard = zip squares infAllDigits
+
+parseSquare :: (String, Char) -> Board -> Maybe Board
+parseSquare (s, x) values
+  | x == '.' || x == '0' = return values
+  | isDigit x = assign (digitToInt x) s values
+  | otherwise = fail "not a valid grid"
+
+parseBoard :: String -> Maybe Board
+parseBoard = foldr ((=<<) . parseSquare) (Just emptyBoard) . zip squares
+
+--takes a tuple of functions and a data tuple
+--returns the tuple with the corresponding function applied
+map2 :: (a -> c, b -> d) -> (a, b) -> (c, d)
+map2 (f1, f2) tl = (f1 $ fst tl, f2 $ snd tl)
+
+--takes a regular function, a bool function and a list
+--returns a list that applies the function if 
+mapIf :: (a -> a) -> (a -> Bool) -> [a] -> [a]
+mapIf func bool list = [func v | v <- filter bool list]
+
+--takes 2 maybes and returns a maybe
+maybeOr :: Maybe a -> Maybe a -> Maybe a
+maybeOr a b
+  |isJust a = a
+  |isJust b = b
+  |otherwise = Nothing
+
+--takes list of maybes returns first just item
+firstJust :: [Maybe a] -> Maybe a
+firstJust list
+  |isJust $ head list = head list
+  |otherwise = firstJust $ drop 1 list
+
+-- takes comparable a, list of tuples
+--returns snd of tuple where fst is a
+lookupList :: Eq a => a -> [(a, [b])] -> [b]
+lookupList _ [] = []
+lookupList key ((f, list):rest)
+  |key == f = list
+  |otherwise = lookupList key rest
+
+maybeBind :: Maybe a -> (a -> Maybe b) -> Maybe b
+maybeBind Nothing _ = Nothing
+maybeBind a func = maybe Nothing func a
+
+--attempts to replace y with y' by checking each element of the list
+--fmap maps function onto a functor
+tryReplace :: Eq a => a -> a -> [a] -> Maybe [a]
+tryReplace _ _ [] = Nothing
+tryReplace y y' (x:xs)
+  |x == y = Just (y':xs)
+  |otherwise = fmap (x:) $ tryReplace y y' xs
+
+recursiveReplacement :: [a] -> [a] -> [a] -> Maybe [a]
+recursiveReplacement l1 (y:y':ys) (z:z':zs)
+  |isJust $ tryReplace y z l1 = tryReplace y' z' l1
+  |otherwise = Nothing
